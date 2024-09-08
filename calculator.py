bb2e41ab427012571bb806c66a28c7b4d486aefa
Gokul <imgokulcs@gmail.com>
2024-09-08 09:06:49 +0530

    Add test case for two numbers
    Fix failing test case for single number

--- calculator.py
+++ calculator.py
@@ -2,3 +2,5 @@ def Add(numbers: str) -> int:
     """A python function to add numbers from a string"""
     if not numbers:
         return 0
+
+    return int(numbers)
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -10,3 +10,7 @@ class TestCalculator(unittest.TestCase):
     def test_one_number(self):
         result = Add("1")
         self.assertEquals(result, 1)
+
+    def test_two_numbers(self):
+        result = Add("1,2")
+        self.assertEquals(result, 1)
